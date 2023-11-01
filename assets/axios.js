414a7bf4e92c9ce7bae892afe56ebedc4ed1f29d
Rahim <103603909+RahimMammad44@users.noreply.github.com>
2023-11-02 00:52:09 +0400

    task-done

---
+++ assets/axios.js
@@ -0,0 +1,32 @@
+const API = "https://api.tvmaze.com/shows";
+
+
+const axiosFunction = async () => {
+    try {
+        const response = await axios.get(API);
+        const data = response.data;
+        console.log(data);
+        const mainCardContainer = document.querySelector(".header-main-container");
+
+        let cardsHTML = "";
+
+        data.slice(0, 9).forEach((card) => {
+            cardsHTML += `
+                <div class="main-card">
+                    <img src="${card.image.medium}">
+                    <h3>${card.name}</h3>
+                    <span>${card.rating.average}</span>
+                    <h4>${card.language}</h4>
+                </div>
+            `;
+        });
+
+        mainCardContainer.innerHTML = cardsHTML;
+    } catch (error) {
+        console.log("Error:", error);
+    }
+};
+
+axiosFunction();
+
+
---
+++ assets/fetch.js
@@ -0,0 +1,30 @@
+const API = "https://api.tvmaze.com/shows";
+
+
+const fetchFunction = async () => {
+    try {
+        const response = await fetch(API);
+        const data = await response.json();
+        console.log(data);
+        const mainCardContainer = document.querySelector(".header-main-container");
+
+        let cardsHTML = "";
+
+        data.slice(0, 9).forEach((card) => {
+            cardsHTML += `
+                <div class="main-card">
+                    <img src="${card.image.medium}">
+                    <h3>${card.name}</h3>
+                    <span>${card.rating.average}</span>
+                    <h4>${card.language}</h4>
+                </div>
+            `;
+        });
+
+        mainCardContainer.innerHTML = cardsHTML;
+    } catch (error) {
+        console.log("Error:", error);
+    }
+};
+
+fetchFunction();
